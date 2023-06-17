9ba0c5a0519a28238e742d47787c2f08645a4fae
BronyaFeetLicker <136499173+BronyaFeetLicker@users.noreply.github.com>
2023-06-17 11:31:02 +0700

    Update and rename stuff.js to script.js

---
+++ script.js
@@ -0,0 +1,42 @@
+const text = document.getElementById("head")
+
+const animate = (element,position) => {
+     element.style.transform = `translateX(${position/3}vw)`
+} 
+
+document.addEventListener('scroll', function(e) {
+  lastKnownScrollPosition = window.scrollY;
+  window.requestAnimationFrame(function(){
+    animate(text,lastKnownScrollPosition*.2)
+  });
+});
+
+
+const popup1 = document.getElementById("myModal1")
+const popup2 = document.getElementById("myModal2")
+const popup3 = document.getElementById("myModal3")
+function pop1(){
+  popup1.style.display="block";
+  window.onclick = function(event) {
+    if (event.target == popup1) {
+      popup1.style.display = "none";
+    }
+  }
+}
+function pop2(){
+  popup2.style.display="block";
+  window.onclick = function(event) {
+    if (event.target == popup2) {
+      popup2.style.display = "none";
+    }
+  }
+}
+function pop3(){
+  popup3.style.display="block";
+  window.onclick = function(event) {
+    if (event.target == popup3) {
+      popup3.style.display = "none";
+    }
+  }
+}
+
