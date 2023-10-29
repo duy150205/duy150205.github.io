7f0489d0d075e133f17c6f724818c1067eafa3b1
duy150205 <136499173+duy150205@users.noreply.github.com>
2023-10-29 13:47:13 +0700

    Update script.js

--- script.js
+++ script.js
@@ -8,12 +8,7 @@ $('body').delay(5000).queue(function () {
 
 
 
-const parallax = document.getElementById("parallax");
 
-window.addEventListener("scroll", function () {
-	let offset = window.pageYOffset;
-	parallax.style.backgroundPositionY = offset * -0.7 + "px";
-});
 
 
 
